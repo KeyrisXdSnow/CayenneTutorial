203349f94647ee1d4d28c986662bf9114b3e2187
kristinatrukhan <4xg9v543X>
2021-04-19 14:21:37 +0300

    generate classes from cayenne

---
+++ src/main/java/org/example/cayenne/persistent/Galary.java
@@ -0,0 +1,9 @@
+package org.example.cayenne.persistent;
+
+import org.example.cayenne.persistent.auto._Galary;
+
+public class Galary extends _Galary {
+
+    private static final long serialVersionUID = 1L;
+
+}
---
+++ src/main/java/org/example/cayenne/persistent/Paiting.java
@@ -0,0 +1,9 @@
+package org.example.cayenne.persistent;
+
+import org.example.cayenne.persistent.auto._Paiting;
+
+public class Paiting extends _Paiting {
+
+    private static final long serialVersionUID = 1L;
+
+}
